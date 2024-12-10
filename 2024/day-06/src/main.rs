862e92303b9a498b2bf4ab693379a108cb8da752
Phena Ildanach <phena.ildanach@gmail.com>
2024-12-10 02:05:01 -0600

    Update 2024 day 06 to use new utils

--- 2024/day-06/src/main.rs
+++ 2024/day-06/src/main.rs
@@ -1,9 +1,12 @@
+use std::collections::HashSet;
 use std::f64::consts::FRAC_PI_2;
 use std::fs;
+use utils::cartesian::Cartesian;
+use utils::grid::Grid;
 
 fn main() {
     let input_file = fs::read_to_string("input").expect("Should be able to read file");
-    let mut start = (0, 0);
+    let mut start = Cartesian::new(0, 0);
     let mut start_found = false;
     let mut layout = input_file.lines()
         .map(|line| {
@@ -11,67 +14,62 @@ fn main() {
                 let caret_index = line.find('^');
                 match caret_index {
                     Some(index) => {
-                        start.1 = index as i16;
+                        start.y = index as i32;
                         start_found = true;
                     }
                     None => {
-                        start.0 = start.0 + 1;
+                        start.x += 1;
                     }
                 }
             }
             line.chars().collect()
         })
-        .collect::<Vec<Vec<char>>>();
-    let map_height = layout.len() as i16;
-    let map_width = layout[0].len() as i16;
-    let out_of_bounds = |position: (i16, i16)| {
-        position.0 < 0 || position.0 >= map_height || position.1 < 0 || position.1 >= map_width
-    };
+        .collect::<Grid<char>>();
 
     let mut position = start;
-    let mut heading = (-1, 0);
+    let mut heading = Cartesian::new(-1, 0);
     loop {
-        layout[position.0 as usize][position.1 as usize] = 'X';
-        let tentative = (position.0 + heading.0, position.1 + heading.1);
-        if out_of_bounds(tentative) {
-            break;
-        }
-        if layout[tentative.0 as usize][tentative.1 as usize] == '#' {
-            heading = rotate_heading(heading);
-        } else {
-            position = tentative;
+        layout.set_coordinate(position, 'X');
+        let tentative = position + heading;
+        match layout.get_coordinate(tentative) {
+            Some('#') => {
+                heading = rotate_heading(heading);
+            },
+            Some(_) => {
+                position = tentative;
+            },
+            None => break
         }
     }
-    let positions_occupied = layout.iter()
+    let positions_occupied = layout.get_data().iter()
         .fold(0, |sum, row| sum + row.iter().filter(|c| **c == 'X').count());
     println!("{}", positions_occupied);
 
     let mut position = start;
-    let mut heading = (-1, 0);
+    let mut heading = Cartesian::new(-1, 0);
     loop {
-        let tentative = (position.0 + heading.0, position.1 + heading.1);
-        if out_of_bounds(tentative) {
+        let tentative = position + heading;
+        let spot = layout.get_coordinate(tentative);
+        if spot == None {
             break;
-        }
-        if layout[tentative.0 as usize][tentative.1 as usize] == '#' {
+        } else if spot == Some('#') {
             heading = rotate_heading(heading);
-        } else if tentative == start || layout[tentative.0 as usize][tentative.1 as usize] == 'O' {
+        } else if tentative == start || spot == Some('O') {
             position = tentative;
         } else {
-            let mut previous_turns = vec![];
+            let mut previous_turns = HashSet::new();
             let mut position2 = start;
-            let mut heading2 = (-1, 0);
+            let mut heading2 = Cartesian::new(-1, 0);
             loop {
-                let tentative2 = (position2.0 + heading2.0, position2.1 + heading2.1);
-                if out_of_bounds(tentative2) {
+                let tentative2 = position2 + heading2;
+                let spot2 = layout.get_coordinate(tentative2);
+                if spot2 == None {
                     break;
-                }
-                if tentative2 == tentative || layout[tentative2.0 as usize][tentative2.1 as usize] == '#' {
-                    if previous_turns.contains(&(position2, heading2)) {
-                        layout[tentative.0 as usize][tentative.1 as usize] = 'O';
+                } else if tentative2 == tentative || spot2 == Some('#') {
+                    if !previous_turns.insert((position2, heading2)) {
+                        layout.set_coordinate(tentative, 'O');
                         break;
                     }
-                    previous_turns.push((position2, heading2));
                     heading2 = rotate_heading(heading2);
                 } else {
                     position2 = tentative2;
@@ -80,13 +78,13 @@ fn main() {
             position = tentative;
         }
     }
-    let obstacle_spots = layout.iter()
+    let obstacle_spots = layout.get_data().iter()
         .fold(0, |sum, row| sum + row.iter().filter(|c| **c == 'O').count());
     println!("{}", obstacle_spots);
 }
 
-fn rotate_heading(heading: (i16, i16)) -> (i16, i16) {
-    let heading = (heading.0 as f64).atan2(heading.1 as f64) + FRAC_PI_2;
+fn rotate_heading(heading: Cartesian) -> Cartesian {
+    let heading = (heading.x as f64).atan2(heading.y as f64) + FRAC_PI_2;
     let (sin, cos) = heading.sin_cos();
-    (sin.round() as i16, cos.round() as i16)
+    Cartesian::new(sin.round() as i32, cos.round() as i32)
 }
