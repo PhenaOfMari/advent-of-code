4b088b6a76309e006015d3e5828010e9a58e72a9
Phena Ildanach <phena.ildanach@gmail.com>
2024-12-23 02:51:54 -0600

    More speed improvements

--- 2024/day-22/src/main.rs
+++ 2024/day-22/src/main.rs
@@ -16,16 +16,12 @@ fn main() {
     });
     println!("{}", sum);
 
-    let (key_set, price_map) = build_price_data(&initial_values);
+    let profit_map = calc_profit(&initial_values);
     let mut max_bananas = 0;
-    for key in key_set.iter() {
-        let bananas = price_map.iter().fold(0, |sum, (_, map)| {
-            match map.get(key) {
-                Some(&p) => sum + p,
-                None => sum
-            }
-        });
-        max_bananas = max_bananas.max(bananas);
+    for (_, &bananas) in profit_map.iter() {
+        if bananas > max_bananas {
+            max_bananas = bananas;
+        }
     }
     println!("{}", max_bananas);
 }
@@ -44,18 +40,17 @@ fn prune(a: u32) -> u32 {
     a & 0xFFFFFF
 }
 
-fn build_price_data(initial_values: &Vec<u32>) -> (HashSet<u32>, HashMap<u32, HashMap<u32, u16>>) {
-    let mut key_set = HashSet::new();
-    let mut price_map = HashMap::new();
+fn calc_profit(initial_values: &Vec<u32>) -> HashMap<u32, u16> {
+    let mut profit_map = HashMap::new();
     for &value in initial_values.iter() {
         let mut current_n = value;
         let mut last_p;
         let mut current_p = price(value);
-        let mut prices = HashMap::new();
         let mut i;
         let mut j= 0;
         let mut k= 0;
         let mut l= 0;
+        let mut seen_keys = HashSet::new();
         for m in 0..2000 {
             current_n = next(current_n);
             last_p = current_p;
@@ -65,14 +60,18 @@ fn build_price_data(initial_values: &Vec<u32>) -> (HashSet<u32>, HashMap<u32, Ha
             k = l;
             l = current_p - last_p;
             let key = gen_key(i as u8, j as u8, k as u8, l as u8);
-            key_set.insert(key);
-            if m > 2 && !prices.contains_key(&key) {
-                prices.insert(key, current_p as u16);
+            if m > 2 && !seen_keys.contains(&key) {
+                seen_keys.insert(key);
+                if profit_map.contains_key(&key) {
+                    let total = profit_map.get(&key).unwrap();
+                    profit_map.insert(key, total + current_p as u16);
+                } else {
+                    profit_map.insert(key, current_p as u16);
+                }
             }
         }
-        price_map.insert(value, prices);
     }
-    (key_set, price_map)
+    profit_map
 }
 
 fn gen_key(i: u8, j: u8, k: u8, l: u8) -> u32 {
